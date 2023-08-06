12a7d85d6c18c178cdf2ba803dfde75bb0a57b47
oXoGus <132100130+oXoGus@users.noreply.github.com>
2023-08-06 20:46:01 +0200

    del
    Signed-off-by: oXoGus <132100130+oXoGus@users.noreply.github.com>

--- TEST/test2.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('CocPlayer.db') # connection a la base de donnée
-cursor = conn.cursor() # creation d'une variable pour interagire avec la base de donnée 
-
-clan_tag = '#2QGJGJ'
-cursor.execute('SELECT clanID FROM Player WHERE clanID = ?', (clan_tag,))
-clanResult = cursor.fetchone()
-print(clanResult)
