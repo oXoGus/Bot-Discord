528c561a0f96ce67fa5dccfbf31f0e9485f80cb7
oXoGus <132100130+oXoGus@users.noreply.github.com>
2023-11-21 11:02:29 +0100

    proto

---
+++ protoSyncroClan.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+# conection to the db 
+
+# add i column 
+
+
+while 1:
+    #try search table with the tag of the clan 
+    #request clan 
+    # request player data
+    # i = i + 1
+    try : 
+        # for player in clanMember    
+        # if player already in the db 
+        # update player json and i
+        # else send join message + insert new player  
+        # cursor.execute("SELECT FROM clanMember_{clanTag} WHERE I = ?", (i-1,))
+        # for player in cursor.fetchall :
+        # send leave message + delet player from the db
+        pass    
+    except : 
+        #  for player in clanMember 
+        # insert player and i 
+        pass
